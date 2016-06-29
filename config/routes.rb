d0a35d1d3bf5237780fa2aeee6bb7992527381b0
DiceUA <tolik.sensei@gmail.com>
2016-06-29 17:49:42 +0300

    Posts added

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   get 'signin' => 'sessions#new'
   delete 'signout' => 'sessions#destroy'
+  resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
