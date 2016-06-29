79dd1d8de74f694eaec48a33e5d9bfbf53507027
DiceUA <tolik.sensei@gmail.com>
2016-06-29 15:34:10 +0300

    Seed Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,79 @@ User.create(
         email: 'gerrard@example.com',
         password: 'butler'
 )
+
+
+Post.create(
+        title: 'First post',
+        content: 'How to read a tome of Collected Poems?
+                  Read one that pivotally changes you
+                  and lose track of the page and title.
+                  How to clean a house? Lose your ring in it.
+
+                  Milosz not having to make peace one day
+                  because the people are dead, nor revisit
+                  some cities of his blood, because they are
+                  razed. I’m still reading for that one.
+
+                  If I wince that I got cuppy, said too much,
+                  maybe years ago, sometimes the sudden
+                  knowledge that my auditor is no longer
+                  will come in as wistful relief, if with grief.
+
+                  So I’d like to find it. This “how” isn’t
+                  an engineering question, but angle,
+                  here alchemically
+                  translated to hope by way of loss.',
+        visible: true,
+        user_id: 1
+)
+
+Post.create(
+    title: 'Second post',
+    content: 'How to read a tome of Collected Poems?
+                  Read one that pivotally changes you
+                  and lose track of the page and title.
+                  How to clean a house? Lose your ring in it.
+
+                  Milosz not having to make peace one day
+                  because the people are dead, nor revisit
+                  some cities of his blood, because they are
+                  razed. I’m still reading for that one.
+
+                  If I wince that I got cuppy, said too much,
+                  maybe years ago, sometimes the sudden
+                  knowledge that my auditor is no longer
+                  will come in as wistful relief, if with grief.
+
+                  So I’d like to find it. This “how” isn’t
+                  an engineering question, but angle,
+                  here alchemically
+                  translated to hope by way of loss.',
+    visible: true,
+    user_id: 2
+)
+
+Post.create(
+    title: 'Third post',
+    content: 'How to read a tome of Collected Poems?
+                  Read one that pivotally changes you
+                  and lose track of the page and title.
+                  How to clean a house? Lose your ring in it.
+
+                  Milosz not having to make peace one day
+                  because the people are dead, nor revisit
+                  some cities of his blood, because they are
+                  razed. I’m still reading for that one.
+
+                  If I wince that I got cuppy, said too much,
+                  maybe years ago, sometimes the sudden
+                  knowledge that my auditor is no longer
+                  will come in as wistful relief, if with grief.
+
+                  So I’d like to find it. This “how” isn’t
+                  an engineering question, but angle,
+                  here alchemically
+                  translated to hope by way of loss.',
+    visible: true,
+    user_id: 1
+)
